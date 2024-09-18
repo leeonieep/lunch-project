0cd6bbc392fffef3e071b9b8e75e9743ecf38fdf
leeonieep <leonie.phelps@awaze.com>
2024-09-18 16:58:30 +0100

    wip

--- LunchProject/Program.cs
+++ LunchProject/Program.cs
@@ -6,8 +6,8 @@ builder.Services.AddSwaggerGen(c =>
 {
     const string title = "Lunch Spots API";
 
-    c.SwaggerDoc("v3",
-        new OpenApiInfo { Title = title, Version = "v3" });
+    c.SwaggerDoc("v1",
+        new OpenApiInfo { Title = title, Version = "v1" });
 });
     
 builder.Services.AddControllers();
@@ -19,8 +19,7 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI(swaggerUiOptions =>
 {
-    swaggerUiOptions.SwaggerEndpoint("/swagger/v3/swagger.json", "Lunch Spots API v3");
-    swaggerUiOptions.SwaggerEndpoint("/swagger/internal-v3/swagger.json", "Lunch Spots API Internal v3");
+    swaggerUiOptions.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
     swaggerUiOptions.RoutePrefix = "docs"; 
 });
 
