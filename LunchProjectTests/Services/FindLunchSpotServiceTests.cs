7f743a744926dffa22f52c0c62113488113c3e1a
leeonieep <leonie.phelps@awaze.com>
2024-10-23 14:56:34 +0100

    wip

--- LunchProjectTests/Services/FindLunchSpotServiceTests.cs
+++ LunchProjectTests/Services/FindLunchSpotServiceTests.cs
@@ -10,7 +10,7 @@ public class FindLunchSpotServiceTests
 {
     private readonly Mock<ILunchSpotRepository> _lunchSpotRepositoryMock;
     private readonly FindLunchSpotService _subjectUnderTest;
-    
+
     public FindLunchSpotServiceTests()
     {
         _lunchSpotRepositoryMock = new Mock<ILunchSpotRepository>();
@@ -24,19 +24,111 @@ public class FindLunchSpotServiceTests
         {
             PriceRange = "$"
         };
-        
+
         var allLunchSpots = new List<LunchSpot>
         {
             new() { Name = "spot one", PriceRange = "$" },
-            new() { Name = "spot two",PriceRange = "$$" },
-            new() { Name = "spot three",PriceRange = "$$$" }
+            new() { Name = "spot two", PriceRange = "$$" },
+            new() { Name = "spot three", PriceRange = "$$$" }
         };
-        
+
         _lunchSpotRepositoryMock.Setup(r => r.LoadFromFile()).ReturnsAsync(allLunchSpots);
-        
+
         var result = await _subjectUnderTest.FindLunchSpot(request);
-        
+
         result.Count.ShouldBe(1);
         result[0].Name.ShouldBe("spot one");
+    }
+
+    [Fact]
+    public async Task FindLunchSpot_ShouldReturnMatchingSpotsWhenAveragePortionSizeIsProvided()
+    {
+        var request = new RequestLunchSpot
+        {
+            AveragePortionSize = "medium"
+        };
+
+        var allLunchSpots = new List<LunchSpot>
+        {
+            new() { Name = "spot one", AveragePortionSize = "small" },
+            new() { Name = "spot two", AveragePortionSize = "medium" },
+            new() { Name = "spot three", AveragePortionSize = "large" }
+        };
+
+        _lunchSpotRepositoryMock.Setup(r => r.LoadFromFile()).ReturnsAsync(allLunchSpots);
+
+        var result = await _subjectUnderTest.FindLunchSpot(request);
+
+        result.Count.ShouldBe(1);
+        result[0].Name.ShouldBe("spot two");
+    }
+
+    [Fact]
+    public async Task FindLunchSpot_ShouldReturnMatchingSpotsWhenMinutesWalkAwayIsProvided()
+    {
+        var request = new RequestLunchSpot
+        {
+            MinutesWalkAway = 10
+        };
+
+        var allLunchSpots = new List<LunchSpot>
+        {
+            new() { Name = "spot one", MinutesWalkAway = 5 },
+            new() { Name = "spot two", MinutesWalkAway = 7 },
+            new() { Name = "spot three", MinutesWalkAway = 15 }
+        };
+
+        _lunchSpotRepositoryMock.Setup(r => r.LoadFromFile()).ReturnsAsync(allLunchSpots);
+
+        var result = await _subjectUnderTest.FindLunchSpot(request);
+
+        result.Count.ShouldBe(2);
+        result[0].Name.ShouldBe("spot one");
+        result[1].Name.ShouldBe("spot two");
+    }
+
+    [Fact]
+    public async Task FindLunchSpot_ShouldReturnMatchingSpotsWhenSuitabilityForIndividualIsProvided()
+    {
+        var request = new RequestLunchSpot
+        {
+            SuitableForJanet = true
+        };
+
+        var allLunchSpots = new List<LunchSpot>
+        {
+            new() { Name = "spot one", SuitableForJanet = true },
+            new() { Name = "spot two", SuitableForJanet = false },
+            new() { Name = "spot three", SuitableForJanet = false }
+        };
+
+        _lunchSpotRepositoryMock.Setup(r => r.LoadFromFile()).ReturnsAsync(allLunchSpots);
+
+        var result = await _subjectUnderTest.FindLunchSpot(request);
+
+        result.Count.ShouldBe(1);
+        result[0].Name.ShouldBe("spot one");
+    }
+
+    [Fact]
+    public async Task FindLunchSpot_ShouldReturnEmptyWhenThereAreNoMatchingSpots()
+    {
+        var request = new RequestLunchSpot
+        {
+            MinutesWalkAway = 5
+        };
+
+        var allLunchSpots = new List<LunchSpot>
+        {
+            new() { Name = "spot one", MinutesWalkAway = 10 },
+            new() { Name = "spot two", MinutesWalkAway = 11 },
+            new() { Name = "spot three", MinutesWalkAway = 20 }
+        };
+
+        _lunchSpotRepositoryMock.Setup(r => r.LoadFromFile()).ReturnsAsync(allLunchSpots);
+
+        var result = await _subjectUnderTest.FindLunchSpot(request);
+
+        result.Count.ShouldBe(0);
     }
 }
