4ccddc9ccaabc704e93d6146e77ad67e37aceb0b
leeonieep <leonie.phelps@awaze.com>
2024-12-04 16:20:13 +0000

    wip

--- LunchProject/Program.cs
+++ LunchProject/Program.cs
@@ -1,9 +1,30 @@
-﻿using LunchProject.Repositories;
+﻿using System.Text;
+using LunchProject.Repositories;
 using LunchProject.Services;
 using LunchProject.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var jwtIssuer = builder.Configuration.GetSection("Jwt:Issuer").Get<string>();
+var jwtKey = builder.Configuration.GetSection("Jwt:Key").Get<string>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtIssuer,
+            ValidAudience = jwtIssuer,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+        };
+    });
 
 builder.Services.AddSwaggerGen(c =>
 {
@@ -37,6 +58,7 @@ app.UseSwaggerUI(swaggerUiOptions =>
 app.UseHttpsRedirection();
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
